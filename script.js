be391cc35114176473c226cea569a3b1fe694971
Jeff Duke <jeff.duke@gmail.com>
2016-07-18 16:12:10 -0600

    complete jquery functionality

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-// on button click generate random color value
-// pass random color value to css class for body background
+var random = $('.random');
+var reset = $('.reset');
+var color = Math.floor((Math.random()* 255) +1);
+
+random.on('click', function() {
+  changeBackgroundColor();
+})
+
+reset.on('click', function() {
+  resetBackgroundColor();
+})
+
+function changeBackgroundColor() {
+  var colorR = Math.floor((Math.random()* 255) +1);
+  var colorG = Math.floor((Math.random()* 255) +1);
+  var colorB = Math.floor((Math.random()* 255) +1);
+  if (colorR != 0 && colorG != 0 && colorB != 0) {
+    $('body').css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB  + ")");
+  }
+  else {
+    changeBackgroundColor();
+  }
+}
+
+function resetBackgroundColor() {
+ $('body').css("background-color", "white");
+}
